ed0fe1c2738cc26618d8bfae890c45c79eee8fb2
bruno <kamipm123@gmail.com>
2024-10-23 23:52:40 -0300

    sadadas

---
+++ app/js/handle.js
@@ -0,0 +1,8 @@
+$(document).ready( () => {
+
+    var createBlocos = "<img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAgMAAABinRfyAAAACVBMVEW9vb3///97e3uVBMaVAAAAHklEQVQI12MIDQ0NARFBDAEMDFzkEl6rVq1i0AISAIlSC03msuDYAAAAAElFTkSuQmCC' alt='xs'>"
+
+    $("#euamojessica").append(createBlocos);
+
+    
+})
