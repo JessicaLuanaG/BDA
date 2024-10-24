93b7c673a9404d4f3b4e1516b52b5a62802452ad
Jessica Luana Gula Dreher <jessicaluanagulad@gmail.com>
2024-10-24 00:05:20 -0300

    ADD

--- app/js/handle.js
+++ app/js/handle.js
@@ -1,8 +1,6 @@
-$(document).ready( () => {
+$(document).ready(() => {
+  var createBlocos =
+    "<img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAgMAAABinRfyAAAACVBMVEW9vb3///97e3uVBMaVAAAAHklEQVQI12MIDQ0NARFBDAEMDFzkEl6rVq1i0AISAIlSC03msuDYAAAAAElFTkSuQmCC' alt='xs'>";
 
-    var createBlocos = "<img src='data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQAgMAAABinRfyAAAACVBMVEW9vb3///97e3uVBMaVAAAAHklEQVQI12MIDQ0NARFBDAEMDFzkEl6rVq1i0AISAIlSC03msuDYAAAAAElFTkSuQmCC' alt='xs'>"
-
-    $("#euamojessica").append(createBlocos);
-
-    
-})
+  $("#euamojessica").append(createBlocos);
+});
